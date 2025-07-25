bb09d97937d7eafffb63b6696b9e5cb13f1b35bf
Aisha Abdullahi <ana9@calvin.edu>
2025-07-25 16:17:29 -0400

    typed js

--- script.js
+++ script.js
@@ -43,10 +43,23 @@ window.onscroll = () => {
 
 /*scroll reveal */
 ScrollReveal({
-    reset: true,
+   // reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .experiences-container, .projects-box,.contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+//typed js
+const typed = new Typed('.multiple-text', {
+    strings: ['Backend Developer', 'Software Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
