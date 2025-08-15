986eee1aed30c612444b8399302269241f9b11d8
Aisha Abdullahi <ana9@calvin.edu>
2025-08-15 13:54:08 -0400

    web dev focus

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 //typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Backend Developer', 'Software Engineer'],
+    strings: ['Junior Backend Developer', 'Junior Web Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
