f6f6ed4e75336965867e73729532d0cb0badb5d8
Aisha Abdullahi <ana9@calvin.edu>
2026-07-08 10:37:30 -0400

    removed junior dev

--- script.js
+++ script.js
@@ -55,8 +55,8 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 //typed js
-const typed = new Typed('.multiple-text', {
-    strings: ['Junior Backend Developer', 'Junior Full-Stack Developer'],
+const typed = new Typed('.single-text', {
+    strings: [ 'Junior Full-Stack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
